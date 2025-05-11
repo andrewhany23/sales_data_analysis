49f17b98d395616628a9db92710db27d6bdffc02
Andrew H. William <andrewhany23@gmail.com>
2025-05-12 00:08:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,28 @@
 import pandas as pd
+import os
 
-all_data = pd.read_csv("./main/all_data.csv")
+all_data = pd.read_csv("./trial/all_data.csv")
 
-def generate_split_data (type):
-    series = all_data[type]
-    lst = list ()
-    for element in series:
-        if element not in lst:
-            lst.append (element)
-            data = all_data[all_data[type] == element]
-            data.to_csv (f"./split data/{type}/{element}_data.csv")
+def generate_split_data(category):
+    os.makedirs (f"./trial/split data/{category}", exist_ok=True)
+    grouped_data = all_data.groupby(f"{category}")
+    for group in grouped_data:
+        group[1].to_csv(f"./trial/split data/{category}/{group[0]}_data.csv")
 
-def generate_collective_date (type):
-    series = all_data[type]
-    lst = list ()
-    dct = {f"{type}": [], "counts": [], "average_sales": [], "total_sales": []}
-    for element in series:
-        if element not in lst:
-            lst.append (element)
-            data = all_data[all_data[type] == element]
-            counts = len(data)
-            average_sales = int (data.sales.mean ())
-            total_sales = data.sales.sum ()
+def generate_collective_data(category):
+    os.makedirs (f"./trial/collective data/{category}", exist_ok=True)
+    grouped_data = all_data.groupby (f"{category}").agg (
+        counts = ("sales", "size"),
+        average_sales = ("sales", "mean"),
+        total_sales = ("sales", "sum")
+    )
 
-            dct [f"{type}"].append(element)
-            dct ["counts"].append (counts)
-            dct ["average_sales"].append(average_sales)
-            dct ["total_sales"].append(total_sales)
+    grouped_data.to_csv(f"./trial/collective data/{category}/{category}_data.csv")
 
-    data_frame = pd.DataFrame(dct)
-    data_frame.to_csv (f"./collective data/{type}/{type}_data.csv")
+generate_split_data("date")
+generate_split_data("location")
+generate_split_data("payment_method")
 
-generate_split_data ("date")
-generate_split_data ("location")
-generate_split_data ("payment_method")
-
-generate_collective_date ("location")
-generate_collective_date ("date")
-generate_collective_date ("payment_method")
+generate_collective_data("location")
+generate_collective_data("date")
+generate_collective_data("payment_method")
